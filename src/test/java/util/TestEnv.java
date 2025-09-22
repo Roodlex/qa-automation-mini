b9a9349b79debeb7d1dd8e1beb63ebd2dbb67be1
Abhishek mishra <148237402+Roodlex@users.noreply.github.com>
2025-09-23 05:15:39 +0530

    Update TestEnv.java

--- src/test/java/util/TestEnv.java
+++ src/test/java/util/TestEnv.java
@@ -2,6 +2,8 @@ package util;
 
 public final class TestEnv {
     private TestEnv() {}
+
+    /** True when running on GitHub Actions. */
     public static boolean isCi() {
         String ci = System.getenv("CI");
         return ci != null && ci.equalsIgnoreCase("true");
