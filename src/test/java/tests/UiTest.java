29b404c175fd5b177fe89545741a40b8e8f3facf
Abhishek mishra <148237402+Roodlex@users.noreply.github.com>
2025-09-23 04:54:56 +0530

    Update UiTest.java

--- src/test/java/tests/UiTest.java
+++ src/test/java/tests/UiTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,9 +13,15 @@ public class UiTest {
 
     @BeforeClass
     public void setup() {
-        WebDriverManager.chromedriver().setup();
+        // Use Chrome installed by GitHub Actions (set in ci.yml)
+        String chromeBin = System.getenv("CHROME_BIN");
         ChromeOptions options = new ChromeOptions();
+        if (chromeBin != null && !chromeBin.isBlank()) {
+            options.setBinary(chromeBin);
+        }
         options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+
+        // Selenium 4 will resolve the matching driver automatically
         driver = new ChromeDriver(options);
     }
 
